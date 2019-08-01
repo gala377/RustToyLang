fc5b949eda57a1192cb132d5b6259fe35e587f22
Rafał Galczak <r.galczak@gmail.com>
2019-08-02 00:13:57 +0200

    liftl_utility module documentation

--- libftl_utility/src/lib.rs
+++ libftl_utility/src/lib.rs
@@ -1,5 +1,9 @@
-#[macro_use]
-pub mod macros;
+//! Generic utility structures and functions. 
+//! 
+//! Items in this module can be related to
+//! the FTL project but not to the specific 
+//! part of it for example: 
+//! AST factory for testing purposes.
 
 use std::{
     rc::Rc,
@@ -8,10 +12,86 @@ use std::{
     clone::Clone,
 };
 
+
+#[macro_use]
+pub mod macros;
+
+
+/// Newtype for the `Rc<RefCell<T>>`.
+/// 
+/// Expresses mutable object with shared ownership. 
+/// Used mostly because writing `Rc<RefCell<T>>` is tedious.
+/// 
+/// Mind that because the internals use 
+/// [`RefCell`](https://doc.rust-lang.org/std/rc/struct.Rc.html)
+/// to allow for the mutability of the contents, the borrowing rules
+/// are enforced on the runtime rather than the compilation
+/// so borrowing can panic. As with the 
+/// [`RefCell`](https://doc.rust-lang.org/std/cell/struct.RefCell.html)
+/// [`try_borrow`](https://doc.rust-lang.org/std/cell/struct.RefCell.html#try_borrow)
+/// and 
+/// [`try_borrow_mut`](https://doc.rust-lang.org/std/cell/struct.RefCell.html#try_borrow_mut)
+/// might be used to avoid unexpected panics.
+/// 
+/// Implements 
+/// [`Deref`](https://doc.rust-lang.org/std/ops/trait.Deref.html)
+/// trait and dereferences to 
+/// [`RefCell`](https://doc.rust-lang.org/std/cell/struct.RefCell.html) 
+/// so that the 
+/// [`borrow`](https://doc.rust-lang.org/std/cell/struct.RefCell.html#borrow_mut)
+/// and 
+/// [`borrow_mut`](https://doc.rust-lang.org/std/cell/struct.RefCell.html#borrow_mut)
+/// methods can be called directly on the object.
+/// 
+/// Implements 
+/// [`Clone`](https://doc.rust-lang.org/std/clone/trait.Clone.html)
+/// trait which creates new 
+/// instance with cloned 
+/// [`Rc`](https://doc.rust-lang.org/std/rc/struct.Rc)
+/// so it works just like normal 
+/// [`Rc`](https://doc.rust-lang.org/std/rc/struct.Rc) 
+/// would.
+/// 
+/// # Examples
+/// 
+/// Creating new RcRef and cloning sharing ownership:
+/// 
+/// ```
+/// use ftl_utility::RcRef;
+/// 
+/// let counter: RcRef<usize> = RcRef::new(0);
+/// let shared_counter = counter.clone();
+/// ```
+/// 
+/// Borrowing content: 
+/// 
+/// ```
+/// # use ftl_utility::RcRef;
+/// #
+/// # let counter: RcRef<usize> = RcRef::new(0);
+/// # let shared_counter = counter.clone();
+/// {
+///     let mut val = shared_counter.borrow_mut();
+///     *val += 1;
+/// }
+/// {
+///     let mut val = counter.borrow_mut();
+///     *val += 1;
+/// }
+/// ```
 pub struct RcRef<T>(Rc<RefCell<T>>);
 
 impl<T> RcRef<T> {
 
+    /// Creates new `RcRef` with its content 
+    /// being `val`.
+    /// 
+    /// # Examples 
+    /// ```
+    /// use ftl_utility::RcRef;
+    /// 
+    /// let rc_ref = RcRef::new(0);
+    /// ```
     pub fn new(val: T) -> Self {
         Self(Rc::new(RefCell::new(val)))
     }
@@ -28,6 +108,7 @@ impl<T> Deref for RcRef<T> {
 
 } 
 
+// FIXME: Trivial implementation. Possibly could be derived.
 impl<T> Clone for RcRef<T> {
 
     fn clone(&self) -> Self {
--- libftl_utility/src/macros.rs
+++ libftl_utility/src/macros.rs
@@ -1,11 +1,38 @@
+//! Definitions of helpful macros.
 
+
+/// Fails if the expression doesn't match 
+/// any of the provided patterns. 
+/// 
+/// Patterns are checked in the same order as 
+/// they were passed to the macro.
+/// 
+/// # Arguments:
+/// 
+/// * `e` - Expression to be matched.
+/// * `p` - One or more patterns to match the expression against.
+/// 
+/// # Examples
+/// Matching on the `Option`: 
+/// 
+/// ```rust,should_panic
+/// # #[macro_use] extern crate ftl_utility;
+/// # use ftl_utility::assert_match;
+/// # fn main() {
+/// let val = Some(5);
+/// 
+/// assert_match!(val, None, Some(5)); // matches Some(5) or None
+/// assert_match!(val, None); // this fails
+/// assert_match!(val, Some(_)); // this passes
+/// # }
+/// ```
 #[macro_export]
 macro_rules! assert_match {
-    ($e:expr, $( $p:pat )+) => {
+    ($e:expr $(, $p:pat)+) => {
         assert!(match $e {
             $(
                 $p => true,
-            ),*
+            )*
             _ => false,
         });
     }
@@ -24,6 +51,12 @@ mod tests {
     fn match_on_enums() {
         let opt: Option<u8> = None;
         assert_match!(opt, None);
+    }
+
+    #[test]
+    fn match_multiple_patterns() {
+        let opt: Option<u8> = None;
+        assert_match!(opt, Some(1), None);
     }
 
     #[test]
